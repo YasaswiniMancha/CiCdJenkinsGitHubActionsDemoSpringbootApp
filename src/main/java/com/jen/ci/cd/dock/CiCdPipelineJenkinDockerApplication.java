e1190f901e2c5e97335c36130b372fd6639d5553
YasaswiniMancha <yasaswinigayathri@gmail.com>
2025-09-09 23:25:57 +0530

    first cicd

---
+++ src/main/java/com/jen/ci/cd/dock/CiCdPipelineJenkinDockerApplication.java
@@ -0,0 +1,13 @@
+package com.jen.ci.cd.dock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdPipelineJenkinDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdPipelineJenkinDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jen/ci/cd/dock/CiCdPipelineJenkinDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jen.ci.cd.dock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdPipelineJenkinDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
